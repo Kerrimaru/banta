e27d94fc9ea1b6eacef8140ad006c481aa974e8d
@amy_allen_2866 <allen.amylouise@gmail.com>
2018-04-30 17:20:22 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name:'Amy', email:'amy@email.com' password:'pass123')
+User.create(name:'Kitty', email:'kitty@email.com' password:'pass123')
+User.create(name:'Jordan', email:'Jordan@email.com' password:'pass123')
+User.create(name:'Sheraa', email:'power@email.com' password:'pass123')
+User.create(name:'Lettuice', email:'saladfingers@email.com' password:'pass123')
+
+Room.create(name:'Kawaii Culture')
+Room.create(name:'Hello Kitty')
+Room.create(name:'Small Furry Pets')
