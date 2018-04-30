966d1f0330f917918439a1154f343e7d7431fc33
Kerri <kerri.maru@gmail.com>
2018-04-30 17:15:59 +0100

    added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = [
+  {name:"Bella", password: "pass123", email: "bella@dog.com"},
+  {name:"Userman", password: "pass123", email: "user@userman"},
+  {name:"Vikram", password: "pass123", email: "vik@hotguy"}
+]
+
+users = User.create!(users)
+
+rooms = {
+  {name: "Dogs"},
+  {name: "Bikes"},
+  {name: "Rooms"}
+}
+
+
+rooms = Room.create!
