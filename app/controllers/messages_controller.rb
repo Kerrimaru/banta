eefd5918244d2034b54f52c6ef1b78148aed3baf
Kerri <kerri.maru@gmail.com>
2018-05-03 13:18:43 +0100

    can post a message

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,24 @@
+class MessagesController < ApplicationController
+
+  def create
+    @room = Room.find(params[:id].to_i)
+    @user = current_user
+    @message = Message.new(message_params)
+    @message.user = current_user
+    @message.room_id = @room.id
+    @messages = Message.all   
+    if @message.save
+      redirect_to "/rooms/#{@room.id}"
+    else 
+      render 'rooms/show'
+    end
+  end
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :room)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   resources :rooms, :users, :messages
 
-  # resources :users, :rooms
-
   root to: "rooms#index"
+
+  post '/rooms/:id', to: 'messages#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
